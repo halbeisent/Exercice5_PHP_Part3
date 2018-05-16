fa60413e86bf63c844f89f81419f26eac2c534c2
halbeisent <dev@halbeisen.fr>
2018-05-16 16:08:26 +0200

    Exercice 5 Partie 3 PHP Terminé et Corrigé

--- index.php
+++ index.php
@@ -1,19 +1,15 @@
 <!DOCTYPE html>
 <html lang="fr">
-  <head>
-    <meta charset="utf-8" />
-    <link rel="stylesheet" href="style.css" />
-    <title>Exercice 5 Partie 3 PHP</title>
-  </head>
-  <body>
+<head>
+  <meta charset="utf-8" />
+  <link rel="stylesheet" href="style.css" />
+  <title>Exercice 5 Partie 3 PHP</title>
+</head>
+<body>
     <?php
-    // On initialise la variable $count
-    $count = 0;
-    while ($count <= 15) { //Tant que la valeur de $count est inférieure à 15,
-      echo 'On y arrive presque <br />'; //On affiche ce message,
-      $count++; //Puis on incrémente la variable
-      var_dump($count); //Utilisé pour diagnostic
-    }
-    ?>
-  </body>
+    // Tant que la valeur de $count est inférieure ou égale à 15, on affiche le message ci-dessous:
+    for ($count = 1; $count <= 15; $count++) { ?>
+      <p><?= 'On y arrive presque';?></p>
+    <?php } ?>
+</body>
 </html>
